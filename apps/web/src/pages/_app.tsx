391972393ed3c1f53a54993176484b9ca0b142a9
Jason Kurian <JaKXz@users.noreply.github.com>
2023-04-11 17:38:05 -0400

    apply Roboto font globally

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,11 +2,25 @@ import "uno.css";
 import "../css/reset.css";
 
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="max-w-2xl mx-auto">
-      <Component {...pageProps} />
-    </main>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+
+      <main className="mx-auto max-w-4xl">
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
